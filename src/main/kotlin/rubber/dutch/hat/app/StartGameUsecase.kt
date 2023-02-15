6f27b1d5fdb409d56373d682e277bf5cccc38105
mloony <mloony@hotmail.com>
2023-02-16 02:58:13 +0300

    HAT-37 Модификация части распределения по командам

--- src/main/kotlin/rubber/dutch/hat/app/StartGameUsecase.kt
+++ src/main/kotlin/rubber/dutch/hat/app/StartGameUsecase.kt
@@ -7,8 +7,27 @@ import rubber.dutch.hat.domain.exception.GameNotFoundException
 import rubber.dutch.hat.domain.model.GameId
 import rubber.dutch.hat.domain.port.GameSaver
 import rubber.dutch.hat.domain.service.GameProvider
-import java.util.*
 
+const val teamSize = 2
+
+/**
+ * Примеры распределения игроков по командам
+ *
+ * Для 5 игроков, команда из 2х игроков:
+ * playerIndex=0 teamId=1
+ * playerIndex=1 teamId=1
+ * playerIndex=2 teamId=2
+ * playerIndex=3 teamId=2
+ * playerIndex=4 teamId=2
+ *
+ * Для 4 игроков, команда из 2х игроков:
+ * playerIndex=0 teamId=1
+ * playerIndex=1 teamId=1
+ * playerIndex=2 teamId=2
+ * playerIndex=3 teamId=2
+ *
+ * Метод +- сносно работает для команды из 3х игроков и более, но есть нюансы:)
+ */
 @Component
 class StartGameUsecase(
     private val gameProvider: GameProvider,
@@ -18,20 +37,24 @@ class StartGameUsecase(
     fun execute(gameId: GameId): GameResponse {
         val game = gameProvider.findById(gameId) ?: throw GameNotFoundException()
 
-        var counter = 2
-        var teamId = 1;
-        game.players.shuffled().withIndex().forEach {
-            if (counter > 0) {
-                it.value.teamId = teamId.toString()
-                counter--
-            } else {
-                if (it.index < game.players.size - 1) {
+        var teamId = 0
+        //TODO: можно убрать дублирование кода
+        if (game.players.size % teamSize != 0) {
+            //Количество игроков НЕ делится на количество игроков в команде без остатка
+            game.players.shuffled().withIndex().forEach {
+                if (it.index % teamSize == 0
+                    && it.index != (game.players.size - 1)) { //В таком случае последний игрок попадает в ту же команду что и предыдущие 2 игрока
                     teamId++
-                    counter = 1
-                    it.value.teamId = teamId.toString()
-                } else {
-                    it.value.teamId = teamId.toString()
                 }
+                it.value.teamId = teamId.toString()
+            }
+        } else {
+            //Количество игроков делится на количество игроков в команде без остатка
+            game.players.shuffled().withIndex().forEach {
+                if (it.index % teamSize == 0) {
+                    teamId++
+                }
+                it.value.teamId = teamId.toString()
             }
         }
 
