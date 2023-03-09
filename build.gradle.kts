c378212e512d960d7992a6c1ea377492f0adae89
OrlovAnton88 <orlov88@gmail.com>
2023-03-09 14:41:55 +0300

    HAT-72 Версионирование

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
 	kotlin("jvm") version "1.7.21"
 	id("io.gitlab.arturbosch.detekt") version "1.22.0"
 	id("jacoco")
+	id("com.palantir.git-version") version "1.0.0"
 }
 
 allOpen {
@@ -16,7 +17,8 @@ allOpen {
 }
 
 group = "rubber.dutch.hat"
-version = "0.0.1-SNAPSHOT"
+val gitVersion: groovy.lang.Closure<String> by extra
+version = gitVersion()
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
@@ -58,6 +60,10 @@ tasks.getByName<Jar>("jar") {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.processResources {
+	expand("version" to project.version)
 }
 
 tasks.test {
